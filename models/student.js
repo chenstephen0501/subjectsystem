dffa5870e5b419227608b0c0cb226562396f5a23
Steavn Chen <tom9876555@yahoo.com.tw>
2023-04-04 12:49:58 +0800

    create students model

---
+++ models/student.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Student extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Student.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    account: DataTypes.STRING,
+    address: DataTypes.STRING,
+    avatarImage: DataTypes.STRING,
+    learning: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Student',
+    tableName: 'Students',
+    underscored: true,
+  });
+  return Student;
+};
