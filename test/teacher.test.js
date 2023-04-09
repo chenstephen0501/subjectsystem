28226ba71b4d180d5ac5de11cd6c896137cd70ba
Steavn Chen <tom9876555@yahoo.com.tw>
2023-04-09 16:06:25 +0800

    delete unit-test remove unnecessary code

--- test/teacher.test.js
+++ test/teacher.test.js
@@ -84,17 +84,6 @@ describe('Teachers 路由測試', () => {
           done()
         })
     })
-    // after((done) => {
-    //   api.delete(`/teachers/${t_id}`)
-    //   .expect(200)
-    //   .end((err, res) => {
-    //     if (err) {
-    //       done(err)
-    //     }
-    //     console.log('delete Teacher success.')
-    //     done()
-    //   })
-    // })
   })
 
   context('# GET ', () => {
@@ -132,30 +121,6 @@ describe('Teachers 路由測試', () => {
   })
 
   context('# DELETE ', () => {
-    // before((done) => {
-    //  api.post('/teachers')
-    //     .set('Content-Type', 'application/json')
-    //     .send({
-    //       "name": "joe",
-    //       "phone": "0989898989",
-    //       "account": "joe",
-    //       "password": "joe1",
-    //       "email": "joe@com.com",
-    //       "address": "新竹市香山區食品街50號",
-    //       "avatarImage": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/710.jpg",
-    //       "working": true
-    //     })
-    //     .expect(200)
-    //     .end((err, res) => {
-    //       if (err) {
-    //         done(err)
-    //       }
-    //       t_id = res.body.id
-    //       console.log('create Teacher success.')
-    //       done()
-    //     })
-    // })
-
     it('刪除一位教師資料 DELETE /teachers/:t_id', (done) => {
       api.delete(`/teachers/${t_id}`)
         .expect(200)
